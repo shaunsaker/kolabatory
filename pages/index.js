2a2d78fd4171e11d1723246f1fd5926d9bddfab4
shaunsaker <shaun@aux.co.za>
2019-01-03 16:54:58 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+Home.propTypes = {};
+Home.defaultProps = {};
+
+export default Home;
